1f524fc0aa64678cc31bfeba4bf51f457163d88f
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-18 22:26:30 +0900

    Update store.js
    imported saveStatePlugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
   root: true,
   env: {
+    es6: true,
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard', 'prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
