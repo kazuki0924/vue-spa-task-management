960bb9dc61e24fe27f790c0096603171daaded26
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-18 22:48:48 +0900

    Update Task.vue
    added task.name

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="task-view">
-
+    <div class="flex flex-col flex-grow items-start justify-between px-4">
+      {{ task.name }}
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+  export default {}
 </script>
 
 <style>
-.task-view {
-  @apply relative flex flex-row bg-white pin mx-4 m-32 mx-auto py-4 text-left rounded shadow;
-  max-width: 700px;
-}
+  .task-view {
+    @apply relative flex flex-row bg-white pin mx-4 m-32 mx-auto py-4 text-left rounded shadow;
+    max-width: 700px;
+  }
 </style>
